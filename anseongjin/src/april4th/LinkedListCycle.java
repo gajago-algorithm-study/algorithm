cca6ac71314b995ba3b2a59a162fedcbd0a53e5b
seongjin-an <seongjin4us@gmail.com>
2024-05-01 17:12:15 +0900

    #2 LinkedListCycle

--- anseongjin/src/april4th/LinkedListCycle.java
+++ anseongjin/src/april4th/LinkedListCycle.java
@@ -14,43 +14,41 @@ public class LinkedListCycle {
     }
     static class Solution {
         public boolean hasCycle(ListNode head) {
-            Map<Integer, Integer> map = new HashMap<>();
-            int result = 0;
-            int i = 0;
+            Map<String, Integer> map = new HashMap<>();
+            boolean result = false;
+            ListNode n1 = head;
 
-            while (true) {
-                if (head.next == null) {
-                    result = -1;
+            while (n1 != null) {
+                String s = n1.toString();
+                map.computeIfAbsent(s, k -> 0);
+                Integer i = map.get(s);
+                if (i > 0) {
+                    result = true;
                     break;
+                } else {
+                    map.put(s, ++i);
                 }
-                int val = head.val;
-
-                if (map.get(val) != null) {
-                    result = map.get(val);
-                    break;
-                }
-                map.put(val, i);
-                i++;
+                n1 = n1.next;
             }
-            return result != -1;
+            return result;
         }
     }
 
     public static void main(String[] args) {
         // example1
-//        ListNode node0 = new ListNode(3);
-//        ListNode node1 = new ListNode(2);
-//        ListNode node2 = new ListNode(0);
-//        ListNode node3 = new ListNode(-4);
-//
-//        node0.next = node1;
-//        node1.next = node2;
-//        node2.next = node3;
-//        node3.next = node1;
-//
-//        Solution solution = new Solution();
-//        boolean result = solution.hasCycle(node0);
-//        System.out.println("Output: " + result);
+        ListNode node0 = new ListNode(3);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(0);
+        ListNode node3 = new ListNode(-4);
+
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node1;
+
+        Solution solution = new Solution();
+        boolean result = solution.hasCycle(node0);
+        System.out.println("Output: " + result);
 
 
         // example2
@@ -65,9 +63,9 @@ public class LinkedListCycle {
 //        System.out.println("Output: " + result);
 
         // example3
-        ListNode node0 = new ListNode(1);
-        Solution solution = new Solution();
-        boolean result = solution.hasCycle(node0);
-        System.out.println("Output: " + result);
+//        ListNode node0 = new ListNode(1);
+//        Solution solution = new Solution();
+//        boolean result = solution.hasCycle(node0);
+//        System.out.println("Output: " + result);
     }
 }
